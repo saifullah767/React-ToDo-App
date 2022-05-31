8cbb18ccd91c4b8205c2957cde4ead1e1cc0dea2
unknown <skj99667788@gmail.com>
2022-05-31 07:38:25 -0700

    application

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const headerStyle = {
+  padding: '20px 0',
+  lineHeight: '1.5em',
+};
+const Header = () => (
+  <header style={headerStyle}>
+    <h1
+      style={{
+        fontSize: '6rem',
+        fontWeight: '600',
+        marginBottom: '2rem',
+        lineHeight: '1em',
+        color: '#ececec',
+        textAlign: 'center',
+      }}
+    >
+      To-Do List:
+    </h1>
+  </header>
+);
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import TodoContainer from './components/TodoContainer';
 import reportWebVitals from './reportWebVitals';
+import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
